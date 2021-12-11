48fbd0f5065d84248aaef58cb2cb636aacaa9234
King-Gervais <57141003+King-Gervais@users.noreply.github.com>
2021-12-11 00:32:39 +0000

    share button integration
    Co-Authored-By: PostMarlon--- <57006548+PostMarlon@users.noreply.github.com>

--- IPhone/js/scripts.js
+++ IPhone/js/scripts.js
@@ -36,3 +36,16 @@
 //         handler.close();
 //     });
 // });
+
+
+const shareButton = document.querySelector('.share-button');
+const shareDialog = document.querySelector('.share-dialog');
+const closeButton = document.querySelector('.close-button');
+
+shareButton.addEventListener('click', event => {
+  shareDialog.classList.add('is-open');
+});
+
+closeButton.addEventListener('click', event => {
+  shareDialog.classList.remove('is-open');
+});
